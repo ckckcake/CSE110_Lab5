eab43dc7454d449ee73f2a86aa9d83fde9477736
Kevin Lam <klam2965@gmail.com>
2022-04-29 02:01:29 -0700

    Part 3: Room  -> Testing: Update Gradle Config for Testing

--- app/src/main/java/com/example/lab5/TodoListItem.java
+++ app/src/main/java/com/example/lab5/TodoListItem.java
@@ -2,6 +2,17 @@ package com.example.lab5;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Delete;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.RoomDatabase;
+import androidx.room.Update;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -13,15 +24,20 @@ import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.List;
 
+@Entity(tableName = "todo_list_items")
 public class TodoListItem {
     // 1. Public fields.
-    public long id = 0;
+
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+
+    @NonNull
     public String text;
     public boolean completed;
     public int order;
 
     // 2.  Constructor matching fields above.
-    TodoListItem(String text,  boolean completed, int order) {
+    TodoListItem(@NonNull String text,  boolean completed, int order) {
         this.text = text;
         this.completed = completed;
         this.order = order;
@@ -49,5 +65,28 @@ public class TodoListItem {
                 ", completed=" + completed +
                 ", order=" + order +
                 '}';
+    }
+
+    @Dao
+    public interface TodoListItemDao {
+        @Insert
+        long insert(TodoListItem todoListItem);
+
+        @Query("SELECT * FROM 'todo_list_items' WHERE 'id'=:id")
+        TodoListItem get(long id);
+
+        @Query("SELECT * FROM 'todo_list_items' ORDER BY 'order'")
+        List<TodoListItem> getAll();
+
+        @Update
+        int update(TodoListItem todoListItem);
+
+        @Delete
+        int delete(TodoListItem todoListItem);
+    }
+
+    @Database(entities = {TodoListItem.class}, version = 1)
+    public abstract class TodoDatabase extends RoomDatabase {
+        public abstract TodoListItemDao todoListItemDao();
     }
 }
